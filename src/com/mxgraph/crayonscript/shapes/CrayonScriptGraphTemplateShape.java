350356156d057d2129025de1fb66a95d5f9c0b6c
Ron Vergis <ron.vergis@gmail.com>
2021-06-01 19:05:34 -0500

    Create CrayonScriptGraphTemplateShape.java

---
+++ src/com/mxgraph/crayonscript/shapes/CrayonScriptGraphTemplateShape.java
@@ -0,0 +1,31 @@
+package com.mxgraph.crayonscript.shapes;
+
+import com.mxgraph.canvas.mxGraphics2DCanvas;
+import com.mxgraph.view.mxCellState;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class CrayonScriptGraphTemplateShape extends CrayonScriptBasicShape {
+
+    public CrayonScriptGraphTemplateShape() {
+        super();
+    }
+
+    @Override
+    public void paintShape(mxGraphics2DCanvas canvas, mxCellState state) {
+
+        initialize();
+
+        Rectangle stateRect = state.getRectangle();
+        ArrayList<SvgElement> svgElements = svgElementsMap.get(ShapeStructureType.VERTICAL2);
+
+        SvgElement first = svgElements.get(0);
+        SvgElement second = svgElements.get(1);
+        SvgElement third = svgElements.get(2);
+
+        paintRectangle(canvas, scaleRectangle(stateRect, first, first), first.fillColor);
+        paintRectangle(canvas, scaleRectangle(stateRect, first, second), second.fillColor);
+        paintRectangle(canvas, scaleRectangle(stateRect, first, third), third.fillColor);
+    }
+}
