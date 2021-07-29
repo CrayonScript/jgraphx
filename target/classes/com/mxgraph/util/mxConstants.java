888a5bc26d5fd995102db6af2392624fb3fd4be5
Ron Vergis <ron.vergis@gmail.com>
2021-07-29 00:39:30 -0500

    update mx constants shape names

--- target/classes/com/mxgraph/util/mxConstants.java
+++ target/classes/com/mxgraph/util/mxConstants.java
@@ -998,19 +998,19 @@ public class mxConstants
 	 */
 	public static final int FONT_STRIKETHROUGH= 8;
 
-	public static final String CRAYONSCRIPT_SHAPE_RUN_PARALLEL = "Run Parallel";
+	public static final String CRAYONSCRIPT_SHAPE_RUN_PARALLEL = "Parallel";
 
-	public static final String CRAYONSCRIPT_SHAPE_RUN_SEQUENTIAL = "Run Series";
+	public static final String CRAYONSCRIPT_SHAPE_RUN_SEQUENTIAL = "Series";
 
-	public static final String CRAYONSCRIPT_SHAPE_RUN_SINGLE = "Run Single";
+	public static final String CRAYONSCRIPT_SHAPE_RUN_SINGLE = "Run";
 
-	public static final String CRAYONSCRIPT_SHAPE_IF = "If Condition";
+	public static final String CRAYONSCRIPT_SHAPE_IF = "If";
 
-	public static final String CRAYONSCRIPT_SHAPE_ELSE_IF = "Else If Condition";
+	public static final String CRAYONSCRIPT_SHAPE_ELSE_IF = "Else";
 
-	public static final String CRAYONSCRIPT_SHAPE_WHILE = "While Loop";
+	public static final String CRAYONSCRIPT_SHAPE_WHILE = "While";
 
-	public static final String CRAYONSCRIPT_SHAPE_FOR = "For Loop";
+	public static final String CRAYONSCRIPT_SHAPE_FOR = "For";
 
 	public static final String CRAYONSCRIPT_SHAPE_PARALLEL_EXTENSION = "Add Parallel";
 
